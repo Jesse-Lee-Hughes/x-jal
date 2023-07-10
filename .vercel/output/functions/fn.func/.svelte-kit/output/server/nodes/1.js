3c88b7b489bc5408df7761ff7504babf6587b573
Jesse <jesse-lee.hughes@outlook.com>
2023-07-11 09:06:22 +1000

    remove vercel

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.864abcfa.js","_app/immutable/chunks/index.758fa85b.js","_app/immutable/chunks/stores.8331248b.js","_app/immutable/chunks/singletons.2ae0c43c.js"];
-export const stylesheets = [];
-export const fonts = [];
